5f99584f57618bfb8a897cdc16d82ab32c3e3127
Adam Streck <adam.streck@gmail.com>
2011-10-25 02:46:16 +0200

    Visualization now contains class for testing of visible output, Fake simulation is buggy.

---
+++ parasim-visualization/parasim-visualization-processing/src/main/java/org/sybila/parasim/visualization/TestWindow.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sybila.parasim.visualization;
+
+/**
+ *
+ * @author Asan
+ */
+public class TestWindow extends javax.swing.JFrame {
+    
+    TestWindow() {
+        Visualization testVisualization = new Visualization();
+        setSize(818, 642);  
+        testVisualization.init();        
+        getContentPane().add(testVisualization);
+        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        
+        setVisible(true);
+    }
+    
+    public static void main (String [] args) {
+        TestWindow testWindow = new TestWindow();
+    }
+}
