f12487b10b6fe80a5af3d4da7942fdc138651322
papousek <jan.papousek@gmail.com>
2012-02-13 09:11:17 +0100

    fixed checkstyle violations

--- parasim-computation/parasim-computation-simulation/src/main/java/org/sybila/parasim/computation/cycledetection/cpu/ExtremesMode.java
+++ parasim-computation/parasim-computation-simulation/src/main/java/org/sybila/parasim/computation/cycledetection/cpu/ExtremesMode.java
@@ -3,7 +3,7 @@ package org.sybila.parasim.computation.cycledetection.cpu;
 /**
  * Specifies which extreme values are to be remembered.
  *
- * @author <a href="mailto:sven@mail.muni.cz">Sven Dražan</a>
+ * @author <a href="mailto:sven@mail.muni.cz">Sven Drazan</a>
  */
 public enum ExtremesMode {
 
@@ -13,17 +13,17 @@ public enum ExtremesMode {
     EXTREME_BOTH;
 
     public static ExtremesMode fromInt(int status) {
-	switch(status) {
+    switch(status) {
             case 0:
                 return EXTREME_NONE;
             case 1:
-		return EXTREME_MIN;
+        return EXTREME_MIN;
             case 2:
-		return EXTREME_MAX;
+        return EXTREME_MAX;
             case 3:
-		return EXTREME_BOTH;
+        return EXTREME_BOTH;
             default:
-		throw new IllegalStateException("There is no mode corresponding to the number [" + status + "].");
+        throw new IllegalStateException("There is no mode corresponding to the number [" + status + "].");
         }
     }
 
--- parasim-model/parasim-model-trajectory/src/main/java/org/sybila/parasim/model/trajectory/ArrayPoint.java
+++ parasim-model/parasim-model-trajectory/src/main/java/org/sybila/parasim/model/trajectory/ArrayPoint.java
@@ -1,6 +1,5 @@
 package org.sybila.parasim.model.trajectory;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 /**
@@ -8,34 +7,34 @@ import java.util.Iterator;
  */
 public class ArrayPoint extends AbstractPoint {
 
-	private float[] data;
-	private int startIndex;
+    private float[] data;
+    private int startIndex;
 
     public ArrayPoint(float time, float... data) {
         this(time, data, 0, data.length);
     }
     
-	public ArrayPoint(float time, float[] data, int startIndex, int dimension) {
+    public ArrayPoint(float time, float[] data, int startIndex, int dimension) {
         super(dimension, time);
-		if (data == null) {
-			throw new IllegalArgumentException("The parameter [data] is NULL.");
-		}
-		if (startIndex < 0 || startIndex >= data.length) {
-			throw new IllegalArgumentException("The start index is out of the range [0, " + (data.length - 1) + "].");
-		}
-		if (data.length - startIndex < dimension) {
-			throw new IllegalArgumentException("The length of piece of the array doesn't correspond to the dimension.");
-		}
-		this.data = data;
-		this.startIndex = startIndex;
-	}
+        if (data == null) {
+            throw new IllegalArgumentException("The parameter [data] is NULL.");
+        }
+        if (startIndex < 0 || startIndex >= data.length) {
+            throw new IllegalArgumentException("The start index is out of the range [0, " + (data.length - 1) + "].");
+        }
+        if (data.length - startIndex < dimension) {
+            throw new IllegalArgumentException("The length of piece of the array doesn't correspond to the dimension.");
+        }
+        this.data = data;
+        this.startIndex = startIndex;
+    }
     @Override
-	public float getValue(int index) {
-		if (index < 0 || index >= getDimension()) {
-			throw new IllegalArgumentException("The index is out of the range [0, " + (getDimension() - 1) + "].");
-		}
-		return data[startIndex + index];
-	}
+    public float getValue(int index) {
+        if (index < 0 || index >= getDimension()) {
+            throw new IllegalArgumentException("The index is out of the range [0, " + (getDimension() - 1) + "].");
+        }
+        return data[startIndex + index];
+    }
     
     @Override
     public boolean equals(Object o) {
