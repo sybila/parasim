4541ed733b4f16da195e14124e6f5d7a2b110adf
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-02-21 16:48:55 +0100

    Added FileXMLResource.

---
+++ parasim-model/parasim-model-configuration/src/main/java/org/sybila/parasim/model/xml/FileXMLResource.java
@@ -0,0 +1,52 @@
+package org.sybila.parasim.model.xml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Uses files to store/load objects
+ * 
+ * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
+ *
+ * @param <T> Type of object to be stored/loaded.
+ */
+public abstract class FileXMLResource<T extends XMLRepresentable> extends StreamXMLResource<T> {
+    private File target;
+    
+    /**
+     * Set file used to store/load objects.
+     * @param file Target file.
+     */
+    public void setTargetFile(File file) {
+        target = file;
+    }
+    
+    /**
+     * @return File used to store/load objects.
+     */
+    public File getTargetFile() {
+        return target;
+    }
+    
+    @Override
+    protected InputStream openInputStream() throws XMLException {
+        try {
+            return new FileInputStream(target);
+        } catch (FileNotFoundException fnfe) {
+            throw new XMLException("File not found", fnfe);
+        }
+    }
+    
+    @Override
+    protected OutputStream openOutputStream() throws XMLException {
+        try {
+            return new FileOutputStream(target);
+        } catch (FileNotFoundException fnfe) {
+            throw new XMLException("File not found", fnfe);
+        }
+    }
+}
