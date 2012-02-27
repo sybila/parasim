ece25c9c3368e27a3247dfff9825ae0f559e0051
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-02-27 15:35:59 +0100

    Package info for org.sybila.parasim.model.xml.

---
+++ parasim-model/parasim-model-configuration/src/main/java/org/sybila/parasim/model/xml/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides functionality for storing and loading of objects using XML.
+ * 
+ * Contains mainly interfaces and abstract classes, which have to be implemented
+ * for the purposes of particular class being stored. Provides functionality
+ * for storing and loading XML objects specified by DOM (Document Object Model),
+ * including validation.
+ * 
+ * @see org.w3c.dom
+ */
+package org.sybila.parasim.model.xml;
