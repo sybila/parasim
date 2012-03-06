708f95d334b1d10c45108b1b516537cb28f5b395
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-03-06 17:14:12 +0100

    Implemented loading of OrthogonalSpace from XML.

--- parasim-model/parasim-model-trajectory/src/main/java/org/sybila/parasim/model/space/OrthogonalSpaceFactory.java
+++ parasim-model/parasim-model-trajectory/src/main/java/org/sybila/parasim/model/space/OrthogonalSpaceFactory.java
@@ -1,8 +1,12 @@
 package org.sybila.parasim.model.space;
 
+import org.sybila.parasim.model.trajectory.ArrayPoint;
+import org.sybila.parasim.model.trajectory.Point;
 import org.sybila.parasim.model.xml.XMLFormatException;
 import org.sybila.parasim.model.xml.XMLRepresentableFactory;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class OrthogonalSpaceFactory implements
         XMLRepresentableFactory<OrthogonalSpace> {
@@ -13,8 +17,40 @@ public class OrthogonalSpaceFactory implements
 
     @Override
     public OrthogonalSpace getObject(Node source) throws XMLFormatException {
-        // TODO Auto-generated method stub
-        return null;
+        if (!source.getNodeName().equals(SPACE_NAME)) {
+            throw new XMLFormatException(
+                    "Wrong name of space element (expected `" + SPACE_NAME
+                            + "', received `" + source.getNodeName() + "').");
+        }
+        NodeList children = source.getChildNodes();
+        int dimension = children.getLength();
+        float[] min = new float[dimension];
+        float[] max = new float[dimension];
+        for (int index = 0; index < dimension; index++) {
+            Node child = children.item(index);
+            if (child.getNodeName().equals(DIMENSION_NAME)) {
+                NamedNodeMap attr = child.getAttributes();
+                try {
+                    min[index] = Float.valueOf(attr.getNamedItem(ATTRIBUTE_MIN)
+                            .getNodeValue());
+                    max[index] = Float.valueOf(attr.getNamedItem(ATTRIBUTE_MAX)
+                            .getNodeValue());
+                } catch (NumberFormatException nfe) {
+                    throw new XMLFormatException("Illegible number.", nfe);
+                }
+                if (min[index] > max[index]) {
+                    throw new XMLFormatException("The min bound in dimension <"
+                            + index + "> is greater than max bound ("
+                            + min[index] + ">" + max[index] + ").");
+                }
+            } else {
+                throw new XMLFormatException("Unknown element: "
+                        + child.getNodeName());
+            }
+        }
+        Point minBounds = new ArrayPoint(0, min, 0, dimension);
+        Point maxBounds = new ArrayPoint(0, max, 0, dimension);
+        return new OrthogonalSpace(minBounds, maxBounds);
     }
 
 }
