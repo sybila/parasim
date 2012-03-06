d35e58beff5ba671f1079a3af4fd3c0cd5a713ea
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-03-06 14:18:01 +0100

    Corrected formula storage. Now expected to be functional.

--- parasim-model/parasim-model-verification/src/main/java/org/sybila/parasim/model/verification/stl/FormulaResource.java
+++ parasim-model/parasim-model-verification/src/main/java/org/sybila/parasim/model/verification/stl/FormulaResource.java
@@ -7,6 +7,8 @@ import org.sybila.parasim.model.xml.FileXMLResource;
 import org.sybila.parasim.model.xml.XMLException;
 import org.sybila.parasim.model.xml.XMLFormatException;
 import org.sybila.parasim.model.xml.XMLRepresentableFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 /**
@@ -17,7 +19,49 @@ import org.w3c.dom.Node;
  * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
  *
  */
+/*TODO Je potřeba zařídit, aby i výstup byl "encapsulovaný"
+ * 
+ * Možnosti jsou asi dvě:
+ * * zařídit vnitřní třídu, která bude mít jako výstup formuli a bude mít jako root danou formuli
+ *      * musí se přepsat všechny už hotové metody této třídy
+ *      * vyžaduje správné vnitřní uspořádání a vypořádání se s dědičností
+ * * přepsat nějakým způsobem root této třídy
+ *      * musí se rozšířit celá formule (unimplemented)
+ *      * je to trochu hack
+ *      * přepisuje se pouze getRoot a setRoot
+ */
 public class FormulaResource extends FileXMLResource<Formula> {
+    private static final String FORMULA_NAME = "formula";
+    private static class FormulaContainer implements Formula {
+        public Formula rootFormula;
+        
+        @Override
+        public Element toXML(Document doc) {
+            Element form = doc.createElement(FORMULA_NAME);
+            form.appendChild(rootFormula.toXML(doc));
+            return form;
+        }
+        
+        @Override
+        public int getArity() {
+            throw new UnsupportedOperationException("Mockup container.");
+        }
+        
+        @Override
+        public Formula getSubformula(int index) {
+            throw new UnsupportedOperationException("Mockup container.");
+        }
+        
+        @Override
+        public float getTimeNeeded() {
+            throw new UnsupportedOperationException("Mockup container.");
+        }
+        
+        @Override
+        public FormulaType getType() {
+            throw new UnsupportedOperationException("Mockup container.");
+        }
+    }
     private class FormulaContainerFactory implements XMLRepresentableFactory<Formula> {
         @Override
         public Formula getObject(Node source) throws XMLFormatException {
@@ -26,6 +70,22 @@ public class FormulaResource extends FileXMLResource<Formula> {
     }
     
     private PointVariableMapping mapping;
+    private FormulaContainer container;
+    
+    public FormulaResource() {
+        container = new FormulaContainer();
+        super.setRoot(container);
+    }
+    
+    @Override
+    public void setRoot(Formula target) {
+        container.rootFormula = target;
+    }
+    
+    @Override
+    public Formula getRoot() {
+        return container.rootFormula;
+    }
     
     /**
      * Sets variable name-to-index mapping.
