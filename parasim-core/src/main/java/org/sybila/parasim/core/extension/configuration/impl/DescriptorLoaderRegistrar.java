bca69572c4994d33a402cece9f028f1ba3468d75
papousek <jan.papousek@gmail.com>
2012-03-13 14:29:54 +0100

    added extension descriptor mapper to the instances loaded by the extension

--- parasim-core/src/main/java/org/sybila/parasim/core/extension/configuration/impl/DescriptorLoaderRegistrar.java
+++ parasim-core/src/main/java/org/sybila/parasim/core/extension/configuration/impl/DescriptorLoaderRegistrar.java
@@ -6,6 +6,7 @@ import org.sybila.parasim.core.Instance;
 import org.sybila.parasim.core.annotations.Inject;
 import org.sybila.parasim.core.annotations.Observes;
 import org.sybila.parasim.core.event.ManagerStarted;
+import org.sybila.parasim.core.extension.configuration.api.ExtensionDescriptorMapper;
 import org.sybila.parasim.core.extension.configuration.api.ParasimDescriptor;
 import org.sybila.parasim.core.extension.configuration.api.event.ConfigurationLoaded;
 import org.xml.sax.SAXException;
@@ -18,10 +19,13 @@ public class DescriptorLoaderRegistrar {
     @Inject
     private Instance<ParasimDescriptor> parasimDescriptor;
     @Inject
+    private Instance<ExtensionDescriptorMapper> extensionDescriptorMapper;
+    @Inject
     private Event<ConfigurationLoaded> event;
     
     public void loadDescriptor(@Observes ManagerStarted event) throws IOException, SAXException {
         parasimDescriptor.set(ParasimDescriptorImpl.fromXMLFile("parasim.config.file", "parasim.xml"));
+        extensionDescriptorMapper.set(new ExtensionDescriptorMapperImpl());
         this.event.fire(new ConfigurationLoaded());
     }
     
