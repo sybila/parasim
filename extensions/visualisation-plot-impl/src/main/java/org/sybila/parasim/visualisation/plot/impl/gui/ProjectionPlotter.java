2d65b03106c9fd4c4a4e789915533e898e3d7514
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-04-16 16:19:55 +0200

    Trial of ProjectionPlotter moved to different class.

--- extensions/visualisation-plot-impl/src/main/java/org/sybila/parasim/visualisation/plot/impl/gui/ProjectionPlotter.java
+++ extensions/visualisation-plot-impl/src/main/java/org/sybila/parasim/visualisation/plot/impl/gui/ProjectionPlotter.java
@@ -21,7 +21,6 @@ package org.sybila.parasim.visualisation.plot.impl.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -36,18 +35,13 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import org.sybila.parasim.model.ode.DoubleMap;
 import org.sybila.parasim.model.ode.PointVariableMapping;
 import org.sybila.parasim.model.space.OrthogonalSpace;
-import org.sybila.parasim.model.trajectory.ArrayPoint;
-import org.sybila.parasim.model.trajectory.Point;
 import org.sybila.parasim.model.verification.result.AbstractVerificationResult;
-import org.sybila.parasim.model.verification.result.ArrayVerificationResult;
 import org.sybila.parasim.model.verification.result.VerificationResult;
 import org.sybila.parasim.visualisation.plot.api.Plotter;
 import org.sybila.parasim.visualisation.plot.impl.LayerFactory;
 import org.sybila.parasim.visualisation.plot.impl.LayerMetaFactory;
-import org.sybila.parasim.visualisation.plot.impl.layer.OverlapLayer;
 
 /**
  * Plots a 2D projection of a generally n-D space in a window. Exactly two axes
@@ -204,38 +198,5 @@ public class ProjectionPlotter extends JFrame implements Plotter {
     @Override
     public void plot() {
         setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-
-            public void run() {
-                Point[] points = new Point[5];
-                points[0] = new ArrayPoint(0, 0f, 2.3f, 0f);
-                points[1] = new ArrayPoint(0, 3.5f, 4.3f, 0f);
-                points[2] = new ArrayPoint(0, 5.1f, 0.6f, 0.5f);
-                points[3] = new ArrayPoint(0, 4.1f, 1.3f, 0.8f);
-                points[4] = new ArrayPoint(0, 1.8f, 3.8f, 1f);
-                float[] robustness = new float[]{-2.5f, 0.8f, 3.4f, 2.9f, -5.3f};
-                VerificationResult result = new ArrayVerificationResult(5, points, robustness);
-
-                Plotter test = new ProjectionPlotter(result, new TestVariableMapping(), new OverlapLayer(result, AbstractVerificationResult.getEncompassingSpace(result)), new RGCirclePointRenderer());
-                test.plot();
-            }
-        });
-
-    }
-}
-
-class TestVariableMapping extends DoubleMap<Integer> implements PointVariableMapping {
-
-    public TestVariableMapping() {
-        put(new Integer(0), "x");
-        put(new Integer(1), "y");
-        put(new Integer(2), "z");
-    }
-
-    public int getDimension() {
-        return 3;
     }
 }
