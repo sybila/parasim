9f04121337d37a7d9351a4625a53c03451c74f93
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-04-17 13:53:23 +0200

    More general point renderer.

---
+++ extensions/visualisation-plot-impl/src/main/java/org/sybila/parasim/visualisation/plot/impl/render/CirclePointRenderer.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2011 - 2012, Sybila, Systems Biology Laboratory and individual
+ * contributors by the @authors tag.
+ *
+ * This file is part of Parasim.
+ *
+ * Parasim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sybila.parasim.visualisation.plot.impl.render;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import org.sybila.parasim.visualisation.plot.impl.gui.PointRenderer;
+
+/**
+ * Renders point as a circle of a color according to associated robustness.
+ * Circle radius and color of valid and invalid points is specified.
+ * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
+ */
+public class CirclePointRenderer implements PointRenderer {
+
+    private float radius;
+    private Color valid, invalid;
+
+    /**
+     * Specifiy circle radius and color of valid and invalid points.
+     * @param radius Circle radius.
+     * @param valid Color of valid points.
+     * @param invalid Color of invalid points.
+     */
+    public CirclePointRenderer(float radius, Color valid, Color invalid) {
+        this.radius = radius;
+        this.valid = valid;
+        this.invalid = invalid;
+    }
+
+    @Override
+    public void drawPoint(Graphics2D canvas, float x, float y, float robustness) {
+        if (robustness > 0) {
+            canvas.setPaint(valid);
+        } else {
+            canvas.setPaint(invalid);
+        }
+        canvas.fill(new Ellipse2D.Float(x - radius, y - radius, 2 * radius, 2 * radius));
+    }
+}
--- extensions/visualisation-plot-impl/src/main/java/org/sybila/parasim/visualisation/plot/impl/render/RGCirclePointRenderer.java
+++ extensions/visualisation-plot-impl/src/main/java/org/sybila/parasim/visualisation/plot/impl/render/RGCirclePointRenderer.java
@@ -20,28 +20,19 @@
 package org.sybila.parasim.visualisation.plot.impl.render;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
-import org.sybila.parasim.visualisation.plot.impl.gui.PointRenderer;
 
 /**
  * Draws point as a circle. When the robustness is positive (i.e. property is valid),
  * it is green (red otherwise).
  * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
  */
-public class RGCirclePointRenderer implements PointRenderer {
+public class RGCirclePointRenderer extends CirclePointRenderer {
 
     protected static final Color VALID = Color.GREEN;
     protected static final Color INVALID = Color.RED;
     protected static final int RADIUS = 3;
 
-    @Override
-    public void drawPoint(Graphics2D canvas, float x, float y, float robustness) {
-        if (robustness > 0) {
-            canvas.setPaint(VALID);
-        } else {
-            canvas.setPaint(INVALID);
-        }
-        canvas.fill(new Ellipse2D.Float(x - RADIUS, y - RADIUS, 2 * RADIUS, 2 * RADIUS));
+    public RGCirclePointRenderer() {
+        super(RADIUS, VALID, INVALID);
     }
 }
