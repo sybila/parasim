e4cc154013ff769481b52cc684766733c80bb0d0
papousek <jan.papousek@gmail.com>
2012-05-08 18:38:26 +0200

    implemented limited point distance in robustness

--- model/verification/src/main/java/org/sybila/parasim/model/verification/SimpleRobustness.java
+++ model/verification/src/main/java/org/sybila/parasim/model/verification/SimpleRobustness.java
@@ -1,4 +1,29 @@
+/**
+ * Copyright 2011 - 2012, Sybila, Systems Biology Laboratory and individual
+ * contributors by the @authors tag.
+ *
+ * This file is part of Parasim.
+ *
+ * Parasim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.sybila.parasim.model.verification;
+
+import org.sybila.parasim.model.trajectory.Distance;
+import org.sybila.parasim.model.trajectory.EuclideanMetric;
+import org.sybila.parasim.model.trajectory.LimitedDistance;
+import org.sybila.parasim.model.trajectory.Point;
+import org.sybila.parasim.model.trajectory.PointDistanceMetric;
 
 /**
  * @author <a href="mailto:xpapous1@fi.muni.cz">Jan Papousek</a>
@@ -8,6 +33,7 @@ public final class SimpleRobustness implements Robustness {
     private final float time;
     private final float value;
     private Robustness inverted;
+    private static final PointDistanceMetric<Distance> EUCLIDEAN_METRIC = new EuclideanMetric();
 
     public SimpleRobustness(float value) {
         this(value, 0, null);
@@ -41,4 +67,29 @@ public final class SimpleRobustness implements Robustness {
         return inverted;
     }
 
+    @Override
+    public LimitedDistance distance(float[] first, float[] second) {
+        final Distance distance = EUCLIDEAN_METRIC.distance(first, second);
+        return new LimitedDistance() {
+            public boolean isValid() {
+                return distance.value() >= value;
+            }
+            public float value() {
+                return distance.value();
+            }
+        };
+    }
+
+    @Override
+    public LimitedDistance distance(Point first, Point second) {
+        final Distance distance = EUCLIDEAN_METRIC.distance(first, second);
+        return new LimitedDistance() {
+            public boolean isValid() {
+                return distance.value() >= value;
+            }
+            public float value() {
+                return distance.value();
+            }
+        };
+    }
 }
