a2435d2274c1f8a01adca7ab7c4da89c08fef6fa
papousek <jan.papousek@gmail.com>
2012-06-26 10:28:01 +0200

    added parent to submodules

--- build-in-steps.sh
+++ build-in-steps.sh
@@ -3,7 +3,7 @@ MAVEN_OPTS="-Xmx512m -XX:MaxPermSize=128m";
 SCRIPT_DIR=`dirname $BASH_SOURCE`;
 SCRIPT_DIR=`readlink -f $SCRIPT_DIR`;
 mvn $@ -Plicense -f $SCRIPT_DIR/pom.xml && \
-mvn $@ -f $SCRIPT_DIR/parent/pom.xml && \
+mvn $@ -Pparent -f $SCRIPT_DIR/pom.xml && \
 mvn $@ -Putil -f $SCRIPT_DIR/pom.xml && \
 mvn $@ -Pcore -f $SCRIPT_DIR/pom.xml && \
 mvn $@ -Pmodel -f $SCRIPT_DIR/pom.xml && \
