95d47af5ccb8635a1f0fdf2922f46bd4f8eb8d50
Tomáš Vejpustek <thomas@thomas-ntb.(none)>
2012-07-20 22:17:13 +0200

    Script for executing parasim.

---
+++ parasim-cli.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+JAVA_BIN=java
+TARGET=applications/parasim-cli/target/parasim-cli-1.0.0-SNAPSHOT-dist.jar
+if [ -f $TARGET ]; then
+	$JAVA_BIN -jar $TARGET
+else
+	echo "Parasim has not been built. Try \`build-all install'."
+	exit 1
+fi
