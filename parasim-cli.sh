7b52c3a536f38294429eb4478e70b153bfcec73d
papousek <jan.papousek@gmail.com>
2012-07-30 11:26:43 +0200

    also performence clasifier is considered

--- parasim-cli.sh
+++ parasim-cli.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 JAVA_BIN=java
-TARGET=$(dirname $(readlink -f $0))/applications/parasim-cli/target/parasim-cli-1.0.0-SNAPSHOT-dist.jar
-if [ -f $TARGET ]; then
+TARGET_DIST=$(dirname $(readlink -f $0))/applications/parasim-cli/target/parasim-cli-1.0.0-SNAPSHOT-dist.jar
+TARGET_PERF=$(dirname $(readlink -f $0))/applications/parasim-cli/target/parasim-cli-1.0.0-SNAPSHOT-performence.jar
+if [ -f $TARGET_DIST ]; then
 	$JAVA_BIN -jar $TARGET "$@"
 else
-	echo "Parasim has not been built. Try \`build-all install'."
-	exit 1
+	if [ -f $TARGET_PERF ]; then
+		$JAVA_BIN -jar $TARGET_PERF "$@"
+	else
+		echo "Parasim has not been built. Try \`build-all install'."
+		exit 1
+	fi
 fi
