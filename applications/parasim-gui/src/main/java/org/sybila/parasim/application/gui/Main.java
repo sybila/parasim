d2bfb9a3b512dfd4496049403bcdbb12f0a12842
Jan Papoušek <jan.papousek@gmail.com>
2012-11-05 18:45:02 +0100

    added parasim-gui module

---
+++ applications/parasim-gui/src/main/java/org/sybila/parasim/application/gui/Main.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2011 - 2012, Sybila, Systems Biology Laboratory and individual
+ * contributors by the @authors tag.
+ *
+ * This file is part of Parasim.
+ *
+ * Parasim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sybila.parasim.application.gui;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.IOException;
+import org.sybila.parasim.core.Manager;
+import org.sybila.parasim.core.ManagerImpl;
+import org.sybila.parasim.core.annotations.Default;
+import org.sybila.parasim.extension.projectManager.api.ProjectManager;
+
+/**
+ * @author <a href="mailto:xpapous1@fi.muni.cz">Jan Papousek</a>
+ * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException, Exception {
+        final Manager manager = ManagerImpl.create();
+        manager.start();
+        ProjectManager projectManager = manager.resolve(ProjectManager.class, Default.class, manager.getRootContext());
+        projectManager.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                manager.shutdown();
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+            }
+        });
+        projectManager.setVisible(true);
+    }
+}
