2a86d80e23dc02c6c7e1a55ea810605257fe9eb2
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2012-12-02 17:25:27 +0100

    More detailed time format.

--- extensions/progress-logger/src/main/java/org/sybila/parasim/extension/progresslogger/view/StyledLoggerOutput.java
+++ extensions/progress-logger/src/main/java/org/sybila/parasim/extension/progresslogger/view/StyledLoggerOutput.java
@@ -4,18 +4,18 @@
  *
  * This file is part of Parasim.
  *
- * Parasim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Parasim is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.sybila.parasim.extension.progresslogger.view;
 
@@ -43,7 +43,7 @@ import org.sybila.parasim.extension.progresslogger.api.LoggerOutput;
  */
 public class StyledLoggerOutput extends JScrollPane implements LoggerOutput {
 
-    private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+    private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
     private static final String DEFAULT_STYLE = "MESSAGE";
     private final StyledDocument output;
 
