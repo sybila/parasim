c05efa73387798afd7c97dcf6a6d09423d9be134
Jan Papoušek <jan.papousek@gmail.com>
2013-03-04 13:48:27 +0100

    license headers

--- application/src/main/java/org/sybila/parasim/application/actions/Help.java
+++ application/src/main/java/org/sybila/parasim/application/actions/Help.java
@@ -1,6 +1,21 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2011 - 2013, Sybila, Systems Biology Laboratory and individual
+ * contributors by the @authors tag.
+ *
+ * This file is part of Parasim.
+ *
+ * Parasim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.sybila.parasim.application.actions;
 
@@ -8,8 +23,7 @@ import org.sybila.parasim.application.ParasimOptions;
 import org.sybila.parasim.core.api.Manager;
 
 /**
- *
- * @author jpapouse
+ * @author <a href="mailto:xpapous1@fi.muni.cz">Jan Papousek</a>
  */
 public class Help extends AbstractAction<Void> {
 
