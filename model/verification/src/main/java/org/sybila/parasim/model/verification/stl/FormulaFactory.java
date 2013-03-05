4a8a6059e323c8352e46293b08ef2dcb40ca80ca
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2013-03-05 18:53:33 +0100

    Freeze operator added to formulae. Predicates not yet changed.

--- model/verification/src/main/java/org/sybila/parasim/model/verification/stl/FormulaFactory.java
+++ model/verification/src/main/java/org/sybila/parasim/model/verification/stl/FormulaFactory.java
@@ -4,18 +4,18 @@
  *
  * This file is part of Parasim.
  *
- * Parasim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Parasim is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.sybila.parasim.model.verification.stl;
 
@@ -30,17 +30,23 @@ import org.w3c.dom.NodeList;
 /**
  * Factory creating {@link Formula} objects from XML.
  *
- * In order to load predicates, it contains mapping between variable names and indices.
+ * In order to load predicates, it contains mapping between variable names and
+ * indices.
  *
  * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
  *
  */
 public class FormulaFactory implements XMLRepresentableFactory<Formula> {
 
+    /**
+     * Name of XML attribute index of freeze operator.
+     */
+    public static final String FREEZE_INDEX_NAME = "index";
     private PointVariableMapping mapping;
 
     /**
      * Sets contained variable name-to-index mapping.
+     *
      * @param mapping Mapping between model variable names and indices.
      */
     public FormulaFactory(PointVariableMapping mapping) {
@@ -60,24 +66,29 @@ public class FormulaFactory implements XMLRepresentableFactory<Formula> {
         }
         NodeList children = source.getChildNodes();
 
-        /* PREDICATE */
+        // PREDICATE
         if (type.equals(FormulaType.PREDICATE)) {
             return new LinearPredicateFactory(mapping).getObject(source);
 
-            /* NOT, OR, AND */
+            // NOT, OR, AND, FREEZE
         } else if (type.equals(FormulaType.NOT) || type.equals(FormulaType.AND)
-                || type.equals(FormulaType.OR)) {
+                || type.equals(FormulaType.OR) || type.equals(FormulaType.FREEZE)) {
             if (children.getLength() < 1) {
                 throw new XMLFormatException(
                         "Too few subformulae: 0 (expected at least 1).");
             }
             Formula phi = getObject(children.item(0));
 
-            /* NOT */
+            // NOT
             if (type.equals(FormulaType.NOT)) {
                 return new NotFormula(phi);
 
-                /* OR, AND */
+                //FREEZE
+            } else if (type.equals(FormulaType.FREEZE)) {
+                int index = Integer.valueOf(source.getAttributes().getNamedItem(FREEZE_INDEX_NAME).getNodeValue());
+                return new FreezeFormula(phi, index);
+
+                // OR, AND
             } else {
                 if (children.getLength() < 2) {
                     throw new XMLFormatException(
@@ -85,16 +96,16 @@ public class FormulaFactory implements XMLRepresentableFactory<Formula> {
                 }
                 Formula psi = getObject(children.item(1));
 
-                /* AND */
+                // AND
                 if (type.equals(FormulaType.AND)) {
                     return new AndFormula(phi, psi);
-                    /* OR */
+                    // OR
                 } else {
                     return new OrFormula(phi, psi);
                 }
             }
 
-            /* TEMPORAL FORMULAE */
+            // TEMPORAL FORMULAE
         } else if (type.equals(FormulaType.FUTURE)
                 || type.equals(FormulaType.GLOBALLY)
                 || type.equals(FormulaType.UNTIL)) {
@@ -107,15 +118,15 @@ public class FormulaFactory implements XMLRepresentableFactory<Formula> {
             FormulaInterval interval = new FormulaIntervalFactory().getObject(children.item(0));
             Formula phi = getObject(children.item(1));
 
-            /* FUTURE */
+            // FUTURE
             if (type.equals(FormulaType.FUTURE)) {
                 return new FutureFormula(phi, interval);
 
-                /* GLOBALLY */
+                // GLOBALLY
             } else if (type.equals(FormulaType.GLOBALLY)) {
                 return new GloballyFormula(phi, interval);
 
-                /* UNTIL */
+                // UNTIL
             } else {
                 if (children.getLength() < 3) {
                     throw new XMLFormatException("Too few children: 2 (expected 3).");
---
+++ model/verification/src/main/java/org/sybila/parasim/model/verification/stl/FreezeFormula.java
@@ -0,0 +1,52 @@
+package org.sybila.parasim.model.verification.stl;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * Signal-value freeze operator on formula.
+ *
+ * @author <a href="mailto:xvejpust@fi.muni.cz">Tomáš Vejpustek</a>
+ */
+public class FreezeFormula extends UnaryFormula {
+
+    private int index;
+
+    /**
+     * Create formula by freezing a subformula. Specifies freeze index.
+     *
+     * @param phi Subformula.
+     * @param index Freeze index.
+     */
+    public FreezeFormula(Formula phi, int index) {
+        super(phi);
+        if (index <= 0) {
+            throw new IllegalArgumentException("Frozen time index has to be greater than zero.");
+        }
+        this.index = index;
+    }
+
+    @Override
+    public FormulaType getType() {
+        return FormulaType.FREEZE;
+    }
+
+    @Override
+    public float getTimeNeeded() {
+        return getSubformula(0).getTimeNeeded();
+    }
+
+    /**
+     * @return Freeze index.
+     */
+    public int getFreezeIndex() {
+        return index;
+    }
+
+    @Override
+    public Element toXML(Document doc) {
+        Element target = super.toXML(doc);
+        target.setAttribute(FormulaFactory.FREEZE_INDEX_NAME, Integer.toString(getFreezeIndex()));
+        return target;
+    }
+}
