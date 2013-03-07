67fbd4cf36ac0717f7c4cc9579f46499295ac962
Tomáš Vejpustek <xvejpust@fi.muni.cz>
2013-03-07 18:41:15 +0100

    parasim.sh can be now executed outside of parasim directory.

--- parasim.sh
+++ parasim.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-PARASIM_VERSION=`grep "<version>" pom.xml | head -n 1 | tr -d '</version> '`
 JAVA_BIN=java
 SELF=`readlink -f $0`
 SELF_DIR=`dirname $SELF`
+PARASIM_VERSION=`grep "<version>" "$SELF_DIR/pom.xml" | head -n 1 | tr -d '</version> '`
 TARGET_DIST=$SELF_DIR/application/target/parasim-${PARASIM_VERSION}-dist.jar
 if [ -f $TARGET_DIST ]; then
 	$JAVA_BIN -jar $TARGET_DIST "$@"
