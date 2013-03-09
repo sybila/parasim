4bd35959eafc369edbd89f1f7d69100500b4638a
Jan Papoušek <jan.papousek@gmail.com>
2013-03-09 14:16:37 +0100

    increased minimum number of emitted instances

--- application/src/main/java/org/sybila/parasim/application/model/ValidityRegionsComputation.java
+++ application/src/main/java/org/sybila/parasim/application/model/ValidityRegionsComputation.java
@@ -165,7 +165,7 @@ public class ValidityRegionsComputation implements Computation<VerificationResul
         for (Trajectory t: spawned.getSecondaryTrajectories()) {
             originalSecondaryTrajectories.add(t);
         }
-        int toSpawn = (int) Math.min(Math.ceil(spawned.size() / (float) 30), computationLifecycleConfiguration.getCorePoolSize());
+        int toSpawn = (int) Math.min(Math.ceil(spawned.size() / (float) 30), 2 * computationLifecycleConfiguration.getCorePoolSize());
         int batchSize = (int) Math.ceil(spawned.size() / (float) toSpawn);
         for (int i=0; i<toSpawn; i++) {
             int batchStart = batchSize * i;
