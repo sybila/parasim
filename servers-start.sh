cc14a7b403799cbab3f86b86d4814b34aeeb3ac2
Jan Papoušek <jan.papousek@gmail.com>
2013-04-22 12:41:32 +0200

    servers-start script updated to determine version from pom.xml

--- servers-start.sh
+++ servers-start.sh
@@ -2,6 +2,8 @@
 SELF=`readlink -f $0`
 SELF_DIR=`dirname $SELF`
 CONFIG_FILE=$1
+PARASIM_VERSION=`grep "<version>" "$SELF_DIR/pom.xml" | head -n 1 | tr -d ' ' | sed 's/<version>//g' | sed 's/<\/version>//g'`
+TARGET_DIST=$SELF_DIR/application/target/parasim-${PARASIM_VERSION}-dist.jar
 
 usage()
 {
@@ -21,6 +23,6 @@ if [ "$CONFIG_FILE" == "" ]; then
 fi
 
 for IP in `grep "<value>" parasim.xml | tr '<>' ' ' | awk '{ print $2 }'`; do
-	nohup ssh $IP -t "java -Dparasim.remote.host=$IP -jar $SELF_DIR/application/target/parasim-2.0.0-SNAPSHOT-dist.jar -s" > "log-$IP" &
+	nohup ssh $IP -t "java -Dparasim.remote.host=$IP -jar $TARGET_DIST -s" > "log-$IP" &
 	echo "server $IP started";
 done
