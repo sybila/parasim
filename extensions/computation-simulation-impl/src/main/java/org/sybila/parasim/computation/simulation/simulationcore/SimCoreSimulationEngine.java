ba38e98312eeb02d41080b6f61eb9082b24e8faa
VojtechBruza <vojtech.bruza@gmail.com>
2017-04-07 15:04:41 +0200

    setting parameters and variables in simulation method

--- extensions/computation-simulation-impl/src/main/java/org/sybila/parasim/computation/simulation/simulationcore/SimCoreSimulationEngine.java
+++ extensions/computation-simulation-impl/src/main/java/org/sybila/parasim/computation/simulation/simulationcore/SimCoreSimulationEngine.java
@@ -11,6 +11,7 @@ import org.sybila.parasim.computation.simulation.api.PrecisionConfiguration;
 import org.sybila.parasim.computation.simulation.cpu.SimulationEngine;
 import org.sybila.parasim.model.math.Parameter;
 import org.sybila.parasim.model.math.ParameterValue;
+import org.sybila.parasim.model.math.Variable;
 import org.sybila.parasim.model.ode.OdeSystem;
 import org.sybila.parasim.model.trajectory.*;
 
@@ -28,62 +29,40 @@ public class SimCoreSimulationEngine implements SimulationEngine {
         //TODO
     }
 
-    /*
-    private List<ParameterValue> loadParameterValues(Point point, OdeSystem odeSystem) {
-        List<ParameterValue> paramValues = new ArrayList<>();
-        for (Parameter param : odeSystem.getAvailableParameters().values()) {
-            if (!param.isSubstituted()) {
-                paramValues.add(new ParameterValue(param, point.getValue(param.getIndex())));
-            }
-        }
-        return paramValues;
-    }
-    @Override
-    public Trajectory simulate(Point point, OdeSystem odeSystem, double timeLimit, PrecisionConfiguration precision) {
-        // load parameter values
-        List<ParameterValue> paramValues = loadParameterValues(point, odeSystem);
-        // create substituted octave ode system //Vojta with values loaded from current point
-        OctaveOdeSystem octaveOdeSystem = paramValues.isEmpty() ? new OctaveOdeSystem(odeSystem) : new OctaveOdeSystem(odeSystem.substitute(paramValues));
-        // compute
-        long numOfIterations = Math.round(Math.ceil((1.05 * timeLimit - point.getTime()) / precision.getTimeStep()));
-        if (numOfIterations > getStepLimit()) {
-            throw new IllegalStateException("Can't simulate the trajectory because the number of iterations <" + numOfIterations + "> is higher than the given limit <" + getStepLimit() + ">.");
-        }
-        double[] loadedData = rawSimulation(point, octaveOdeSystem, numOfIterations, precision).getData();
-        float[] data = new float[loadedData.length];
-        for (int dim = 0; dim < octaveOdeSystem.dimension(); dim++) {
-            for (int i = 0; i < loadedData.length / octaveOdeSystem.dimension(); i++) {
-                data[dim + i * octaveOdeSystem.dimension()] = (float) loadedData[dim * (loadedData.length / octaveOdeSystem.dimension()) + i];
-            }
-        }
-        float[] times = new float[loadedData.length / octaveOdeSystem.dimension()];
-        float time = point.getTime();
-        for (int i = 0; i < times.length; i++) {
-            time += precision.getTimeStep();
-            times[i] = time;
-        }
-        if (paramValues.isEmpty()) {
-            return new ArrayTrajectory(data, times, point.getDimension());
-        } else {
-            return new ArrayTrajectory(point, data, times, octaveOdeSystem.dimension());
-        }
-    }
-     */
 
     @Override
     public Trajectory simulate(Point point, OdeSystem odeSystem, double timeLimit, PrecisionConfiguration configuration) {
         Model model = odeSystem.getOriginalModel();
 
-        //DONE how to set model parameters and species values correctly according to current point
-        List<ParameterValue> paramValues = new ArrayList<>();
-        for (Parameter param : odeSystem.getAvailableParameters().values()) { //for each parameter in odesystem
-            if (!param.isSubstituted()) { //if does not have constant value
-                paramValues.add(new ParameterValue(param, point.getValue(param.getIndex()))); //add the value from current point to parameter values
+        System.out.println("PARAMETER VALUES: " + odeSystem.getAvailableParameters() +" VARIABLES: " + odeSystem.getVariables());
+
+        //SETTING VARIABLES
+        for(Variable variable : odeSystem.getVariables().values()){
+            System.out.println(variable.getName() + " - VALUE: " + variable.evaluate(point));
+            System.out.println("INITIAL: " + odeSystem.getInitialVariableValue(variable).getExpression().evaluate(point));
+            if (!variable.isSubstituted()) {
+                //set species (variables) values in model
+                model.getParameter(variable.getName()).setValue(variable.evaluate(point));
             }
         }
-        // TODO Vojta - how to recognize if the index is same as in my Model (corresponding parameters and variables)
+        //SETTING PARAMETERS
+        for(Parameter parameter : odeSystem.getAvailableParameters().values()){
+            if (!parameter.isSubstituted()) {
+                System.out.println(parameter.getName() + " - VALUE: " + parameter.evaluate(point));
+                //set parameters values in model
+                model.getParameter(parameter.getName()).setValue(parameter.evaluate(point));//what is the difference?? "parameter.evaluate(point)" vs "point.getValue(parameter.getIndex())"
+            }
+        }
+        //DONE Vojta - set parameter value in model according to ode system parameter value
 
+        //odeSystem.getAvailableParameters() returns only parameters, use odeSystem.getVariables() to get variables
+        ////TODO Vojta - how does initial conditions setting work (or perturbating over variables not parameters)
+
+        // TODO Vojta - how to recognize if the index is same as in my Model (corresponding parameters and variables)
+        //TODO findOut if parameters are also variables (if it is synonym)
         //TODO create substituted odeSystem (model)
+        // create substituted ode system
+
 
         SBMLinterpreter interpreter = null;
         try {
